2ab2323b5e3d12461ccec830d812e26956c158fd
Andreas Van Loon <andreas.vanloon@student.kdg.be>
2023-06-03 11:17:13 +0200

    Additions

--- src/be/kdg/ExparcSystems/application/TevredenheidsOnderzoekController.java
+++ src/be/kdg/ExparcSystems/application/TevredenheidsOnderzoekController.java
@@ -1,4 +1,8 @@
 package be.kdg.ExparcSystems.application;
 
 public class TevredenheidsOnderzoekController {
+    public TevredenheidsOnderzoek registreerTevredenheidsOnderzoek)(){
+    TevredenheidsOnderzoek tevredenheidsOnderzoek= new TevredenheidsOnderzoek();
+    return tevredenheidsOnderzoek;
+    }
 }
